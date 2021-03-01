468cd469ce30a5f1a91103aa7583744f73f56b35
Felipe Freitas <afelipe.freitas@gmail.com>
2021-03-01 19:05:31 -0300

    Iteration 1 Done

--- server/model/Rooms.model.js
+++ server/model/Rooms.model.js
@@ -1,10 +1,12 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
 const roomSchema = mongoose.Schema({
-  name: { type: String },
-  description: { type: String },
-  imageUrl: { type: String },
-  reviews: [], // we will update this field a bit later when we create review model
+	name: { type: String },
+	description: { type: String },
+	imageUrl: { type: String },
+	reviews: [], // we will update this field a bit later when we create review model
 });
 
-const RoomModel = mongoose.model("Room", roomSchema);
+const RoomModel = mongoose.model('Room', roomSchema);
+
+module.exports = RoomModel;
--- server/routes/rooms.routes.js
+++ server/routes/rooms.routes.js
@@ -1,44 +1,40 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
 
+const Room = require('../model/Rooms.model');
 
-const Room = require("../model/Rooms.model");
-
-router.post("/room", async (req, res) => {
-  
-  try {
-    const newRoom = await Room.create(req.body);
-    return res.status(201).json(newRoom);
-  } catch (err) {
-    return res.status(500).json({ msg: err });
-  }
+router.post('/room', async (req, res) => {
+	try {
+		const newRoom = await Room.create(req.body);
+		return res.status(201).json(newRoom);
+	} catch (err) {
+		return res.status(500).json({ msg: err });
+	}
 });
 
+router.get('/room', async (req, res) => {
+	try {
+		const rooms = await Room.find();
 
-router.get("/room", async (req, res) => {
-  try {
-    const rooms = await Room.find();
-
-    return res.status(200).json(rooms);
-  } catch (err) {
-    return res.status(500).json({ msg: err });
-  }
+		return res.status(200).json(rooms);
+	} catch (err) {
+		return res.status(500).json({ msg: err });
+	}
 });
 
-router.get("/room/:id", async (req, res) => {
-  try {
-    const room = await Room.findOne({ _id: req.params.id });
+router.get('/room/:id', async (req, res) => {
+	try {
+		const room = await Room.findOne({ _id: req.params.id });
 
-    if (!room) {
-      return res.status(404).json({ msg: "Room not found" });
-    }
+		if (!room) {
+			return res.status(404).json({ msg: 'Room not found' });
+		}
 
-    return res.status(200).json(room);
-  } catch (err) {
-    return res.status(500).json({ msg: err });
-  }
+		return res.status(200).json(room);
+	} catch (err) {
+		return res.status(500).json({ msg: err });
+	}
 });
-
 
 // router.put("/room/:id", async (req, res) => {
 //   try {
@@ -58,40 +54,36 @@ router.get("/room/:id", async (req, res) => {
 //   }
 // });
 
-router.patch("/room/:id", async (req, res) => {
-  try {
-    const updatedRoom = await Room.findOneAndUpdate(
-      { _id: req.params.id },
-      { $set: req.body },
-      { new: true }
-    );
+router.patch('/room/:id', async (req, res) => {
+	try {
+		const updatedRoom = await Room.findOneAndUpdate(
+			{ _id: req.params.id },
+			{ $set: req.body },
+			{ new: true }
+		);
 
-    
-    if (!updatedRoom) {
-      return res.status(404).json({ msg: "Room not found" });
-    }
+		if (!updatedRoom) {
+			return res.status(404).json({ msg: 'Room not found' });
+		}
 
-    return res.status(200).json(updatedRoom);
-  } catch (err) {
-    return res.status(500).json({ msg: err });
-  }
+		return res.status(200).json(updatedRoom);
+	} catch (err) {
+		return res.status(500).json({ msg: err });
+	}
 });
 
+router.delete('/room/:id', async (req, res) => {
+	try {
+		const deleted = await Room.deleteOne({ _id: req.params.id });
 
+		if (!deleted) {
+			return res.status(404).json({ msg: 'Room not found' });
+		}
 
-router.delete("/room/:id", async (req, res) => {
-  try {
-    const deleted = await Room.deleteOne({ _id: req.params.id });
-
-    if (!deleted) {
-      return res.status(404).json({ msg: "Room not found" });
-    }
-
-    return res.status(200).json({});
-  } catch (err) {
-    return res.status(500).json({ msg: err });
-  }
+		return res.status(200).json({});
+	} catch (err) {
+		return res.status(500).json({ msg: err });
+	}
 });
-
 
 module.exports = router;
